70baef74d4b68ee58e929df9fbd6c4ed252cb0c3
Cristian Ramirez <50112991+rcristian3@users.noreply.github.com>
2021-12-25 16:35:54 -0600

    feat: page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './webapp/app.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+    bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    @ViewChild('header', {static: true}) header: any;
 
-  constructor() {}
+    constructor(
+        private renderer: Renderer2
+    ) {
+    }
 
+
+    public listenerScroll(event: CustomEvent): void {
+        if(event.detail.scrollTop > 0) {
+            this.renderer.addClass(this.header.el, 'contrast');
+        } else {
+            this.renderer.removeClass(this.header.el, 'contrast');
+        }
+    }
+
+    public scrollTo($element: any) {
+        $element.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest'
+        });
+    }
 }
